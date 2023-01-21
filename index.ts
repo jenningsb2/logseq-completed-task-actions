011abde9b8571ba27aaae4c10da90dc990005a36
Bailey Jennings <jenningsebailey@gmail.com>
2023-01-21 12:10:56 -0500

    intitial-commit

---
+++ index.ts
@@ -0,0 +1,34 @@
+import "@logseq/libs";
+import { getDateForPage } from "logseq-dateutils";
+
+const TASK_MARKERS = new Set(["DONE", "NOW", "LATER", "DOING", "TODO", "WAITING"]);
+
+function main() {
+  logseq.DB.onChanged(async (e) => {
+    const taskBlock = e.blocks.find((block) => TASK_MARKERS.has(block.marker));
+    if (!taskBlock) {
+      return;
+    }
+
+    const hasCompletedProperty = taskBlock.properties?.completed;
+
+    if (taskBlock.marker === "DONE") {
+      if (hasCompletedProperty) {
+        return;
+      }
+
+      const userConfigs = await logseq.App.getUserConfigs();
+      const preferredDateFormat = userConfigs.preferredDateFormat;
+      const datePage = getDateForPage(new Date(), preferredDateFormat);
+
+      logseq.Editor.upsertBlockProperty(taskBlock.uuid, "completed", datePage);
+    } else {
+      if (!hasCompletedProperty) {
+        return;
+      }
+      logseq.Editor.removeBlockProperty(taskBlock.uuid, "completed");
+    }
+  });
+}
+
+logseq.ready(main).catch(console.error);
